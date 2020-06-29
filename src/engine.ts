c762e1a55c93ca96041c0155052bb26fc91c9b2d
Ville Tiikkainen <villetiikkainen@hotmail.fi>
2020-06-30 02:34:36 +0300

    Initial work for getting rid of P5.js

--- src/engine.ts
+++ src/engine.ts
@@ -95,6 +95,7 @@ class Game {
   private _text = "";
   private _ended = false;
   private _frameCount = 0;
+  private _last_timestamp = 0.0; // Time since time origin in milliseconds
 
   private _dots: Array<Array<Color>>;
 
@@ -104,6 +105,9 @@ class Game {
 
   private _gridHeight = 24;
   private _gridWidth = 24;
+
+  private _ctx?: CanvasRenderingContext2D;
+  private _canvasElement? : HTMLCanvasElement;
 
   constructor(config: GameConfig) {
     this._config = config;
@@ -198,141 +202,174 @@ class Game {
    * Calling `run` starts the game.
    */
   run() {
-    // TODO: there's probably a nicer way of expressing this
-    let parentElement = undefined;
-    if (this._config.containerId) {
-      parentElement =
-        document.getElementById(this._config.containerId) || undefined;
+
+    let canvas_width =
+      this._dotSize * this._gridWidth +
+      this._gapSize * (this._gridWidth - 1);
+    let canvas_height =
+      this._dotSize * this._gridHeight +
+      this._gapSize * (this._gridHeight - 1) +
+      50;
+
+    // Get canvas element, or create it if it doesn't exist
+    let canvasElement = <HTMLCanvasElement> document.getElementById("24a2-canvas");
+    if(canvasElement == null) {
+      canvasElement = document.createElement("canvas");
+      canvasElement.width = canvas_width;
+      canvasElement.height = canvas_height;
+      canvasElement.id = "24a2-canvas";
+      // If GameConfig.containerId is set, append child to it
+      // Otherwise, append it to body
+      // @Bug: if script is ran before document is loaded, target element might not be loaded
+      //       so the canvas is appended to body instead, even if containerId is a valid id
+      let parentElement = document.getElementById(this._config.containerId || "") ?? document.body;
+      parentElement.appendChild(canvasElement);
+    }
+    this._canvasElement = canvasElement;
+
+    // Get CanvasRenderingContext2D from the canvasElement
+    let context2D = canvasElement.getContext("2d", { alpha: false } );
+    if(context2D == null) {
+      throw new Error("Couldn't get 2D context from canvas");
+    }
+    this._ctx = context2D;
+
+    // Canvas is created, call user create function
+    if(this._config.create) {
+      this._config.create(this);
     }
 
-    new p5(
-      function(this: Game, p: p5) {
-        p.setup = function(this: Game) {
-          let width =
-            this._dotSize * this._gridWidth +
-            this._gapSize * (this._gridWidth - 1);
-          let height =
-            this._dotSize * this._gridHeight +
-            this._gapSize * (this._gridHeight - 1) +
-            50;
-          p.createCanvas(width, height);
+    // If onDotClicked is provided, add mousedown listener to canvas
+    if(this._config.onDotClicked) {
+      canvasElement.addEventListener("mousedown", (event : MouseEvent) => {
+        const HALFSIZE = this._dotSize / 2;
+        const CELLSIZE = this._dotSize + this._gapSize;
 
-          // Don't draw outlines around circles
-          p.noStroke();
+        // Get mouse canvas coordinates
+        const mx = event.pageX - canvasElement.offsetLeft;
+        const my = event.pageY - canvasElement.offsetTop;
 
-          p.frameRate(this._config.frameRate || 24);
+        // Get dot grid coordinates
+        const gx = Math.floor(mx / CELLSIZE); // Grid-x
+        const gy = Math.floor(my / CELLSIZE); // Grid-y
 
-          if (this._config.create) {
-            this._config.create(this);
-          }
-        }.bind(this);
+        // Early return, grid coordinate is out-of-bounds
+        if(gx < 0 || gx >= this._gridWidth || gy < 0 || gy >= this._gridHeight) {
+          return;
+        }
 
-        p.draw = function(this: Game) {
-          // Set the internal frame count to P5's frame count. This lets us
-          // return the frame count in `getFrameCount`
-          this._frameCount = p.frameCount;
+        // Get dot center canvas coordinates
+        const px = gx * CELLSIZE + HALFSIZE;
+        const py = gy * CELLSIZE + HALFSIZE;
 
-          // If the game has ended, end the P5 iteration and exit.
-          if (this._ended) {
-            p.noLoop();
-            return;
-          }
+        // Get difference between mouse and dot center
+        const dx = px - mx;
+        const dy = py - my;
 
-          // Clear the drawing
-          p.clear();
-          this._clearGrid();
+        // Get square distance from mouse to center of dot
+        // If it's less than square radius, then mouse is inside dot
+        // Could be problematic with really small dots?
+        if(dx * dx + dy * dy < HALFSIZE * HALFSIZE) {
+          // Call user-specified callback function
+          if(this._config.onDotClicked) this._config.onDotClicked(gx, gy);
+        }
+      });
+    }
 
-          if (this._config.update) {
-            this._config.update(this);
-          }
+    // If onKeyPress is provided, add mousedown listener to document
+    // Attaching to the canvas didn't catch any events
+    if(this._config.onKeyPress) {
+      let keypressCallback = this._config.onKeyPress;
+      document.addEventListener("keydown", (event: KeyboardEvent) => {
+        switch(event.keyCode) {
+          case 38: keypressCallback(Direction.Up); return false;
+          case 40: keypressCallback(Direction.Down); return false;
+          case 37: keypressCallback(Direction.Left); return false;
+          case 39: keypressCallback(Direction.Right); return false;
+          default: return true;
+        }
+      });
+    }
 
-          // Draw the grid
-          this._drawGrid(p);
-
-          // Draw the text
-          p.push();
-          p.textFont("monospace");
-          p.textSize(18);
-          let textY =
-            this._dotSize * this._gridHeight +
-            this._gapSize * (this._gridHeight - 1) +
-            32;
-          p.text(this._text, 0, textY);
-          p.pop();
-        }.bind(this);
-
-        p.keyPressed = function(this: Game): boolean {
-          if (!this._config.onKeyPress) {
-            // Return true to not prevent the browser's default behaviour for
-            // this keypress
-            return true;
-          }
-          // TODO: use WASD instead of arrow keys - they don't have a meaning
-          // in the browser
-          if (p.keyCode === p.LEFT_ARROW) {
-            this._config.onKeyPress(Direction.Left);
-            return false;
-          }
-
-          if (p.keyCode === p.RIGHT_ARROW) {
-            this._config.onKeyPress(Direction.Right);
-            return false;
-          }
-
-          if (p.keyCode === p.UP_ARROW) {
-            this._config.onKeyPress(Direction.Up);
-            return false;
-          }
-
-          if (p.keyCode === p.DOWN_ARROW) {
-            this._config.onKeyPress(Direction.Down);
-            return false;
-          }
-          return true;
-        }.bind(this);
-
-        p.mouseClicked = function(this: Game) {
-          if (!this._config.onDotClicked) {
-            return;
-          }
-          const offset = this._dotSize + this._gapSize;
-          // Iterate over all dot locations, and check whether the distance
-          // between the click and the dot centre is less than the dot's
-          // radius
-          for (let y = 0; y < this._dots.length; y++) {
-            let row = this._dots[y];
-            for (let x = 0; x < row.length; x++) {
-              const dx = this._dotSize / 2 + x * offset;
-              const dy = this._dotSize / 2 + y * offset;
-
-              // p.mouseX and p.mouseY give is the coordinates in the canvas
-              // space.
-              const distance = p.dist(dx, dy, p.mouseX, p.mouseY);
-
-              if (distance < this._dotSize / 2) {
-                this._config.onDotClicked(x, y);
-                // We've found the dot, so exit early
-                return;
-              }
-            }
-          }
-        }.bind(this);
-      }.bind(this),
-      parentElement
-    );
+    // Startup the engine loop
+    requestAnimationFrame(this._loop.bind(this));
   }
 
-  private _drawGrid(p: p5) {
+  // Clear canvas by rendering a rect on top of old frame
+  private _clearCanvas() {
+    if(this._ctx && this._canvasElement) {
+      this._ctx.fillStyle = "white";
+      this._ctx.fillRect(0, 0, this._canvasElement.width, this._canvasElement.height);
+    }
+  }
+
+  // Draw text below the grid
+  private _drawText() {
+    if(!this._ctx) return; // Can't draw text without context
+
+    let textY =
+      this._dotSize * this._gridHeight +
+      this._gapSize * (this._gridHeight - 1) +
+      32;
+
+    this._ctx.font = "18px monospace";
+    this._ctx.fillStyle = "black";
+    this._ctx.fillText(this._text, 0, textY);
+  }
+
+  // Engine loop function
+  private _loop(timestamp: number) {
+    if(this._ended) return; // Game has ended, stop the engine loop
+
+    // Request next frame if game hasn't ended
+    requestAnimationFrame(this._loop.bind(this));
+
+    // Framerate check
+    var delta = timestamp - this._last_timestamp;
+    if(delta >= 1000.0 / (this._config.frameRate || 24)) {
+      delta -= (1000.0 / (this._config.frameRate || 24));
+      this._draw();
+      this._last_timestamp = (timestamp - delta); // Offset by remaining delta
+    }
+  }
+
+  private _draw() {
+    // Set the internal frame count
+    this._frameCount += 1;
+    // Clear the canvas
+    this._clearCanvas();
+
+    // Clear the grid
+    this._clearGrid();
+
+    // Call user update if it's set
+    if (this._config.update) {
+      this._config.update(this);
+    }
+
+    // Draw the grid
+    this._drawGrid();
+
+    // Draw the text below the grid
+    this._drawText();
+  }
+
+  // Draw dot grid
+  private _drawGrid() {
+    if(!this._ctx) return; // Can't draw grid without context
+    let ctx = this._ctx;
+
     const offset = this._dotSize + this._gapSize;
-    p.push();
-    p.translate(this._dotSize / 2, this._dotSize / 2);
+    const ds = this._dotSize / 2;
+
     this._dots.forEach((row, y) => {
       row.forEach((dot, x) => {
-        p.fill(p.color(this._getCSSColor(dot)));
-        p.circle(x * offset, y * offset, this._dotSize);
+        ctx.beginPath();
+        ctx.fillStyle = this._getCSSColor(dot);
+        ctx.ellipse(ds + x * offset, ds + y * offset, ds, ds, 0, 0, 2 * Math.PI);
+        ctx.fill();
       });
     });
-    p.pop();
   }
 
   private _clearGrid() {
@@ -369,3 +406,4 @@ class Game {
     }
   }
 }
+
